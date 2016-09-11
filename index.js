8cec0feee7431b956e27e12f7231212f67106aa1
boushi-bird <boushi-bird@users.noreply.github.com>
2016-09-12 03:02:22 +0900

    普通に実装

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello!');
+'use strict';
+
+class Message {
+  constructor(message) {
+    this.message = message;
+  }
+  show() {
+    try {
+      console.log(this.message);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+var m = new Message('Hello!');
+
+m.show();
+// Hello!
+
+var show = m.show;
+show();
+// [TypeError: Cannot read property 'message' of undefined]
+
+setTimeout(m.show, 0);
+// undefined
